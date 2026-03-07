3a17578545818ac13329874f2e79e639276517e8
.\.git <.\.gitignore>
2026-03-07 19:20:09 -0300

    kjkkj

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello, Node.js!");
+import express from 'express';
+
+const NODE_ENV = 'production';
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});

0fc2add3de33b0c1c6ab8b076b6acb81a88be160
.\.git <.\.gitignore>
2026-03-07 20:41:35 -0300

    rdu

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
+
+import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import express from 'express';
+
+// Define the the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// 1. Declaración de variables
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 /**
- * Configure Express middleware
- */
+  * Configure Express middleware
+  */
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
- * Routes
- */
-// Ruta para el Home (página de inicio)
+  * Routes
+  */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-// Ruta para Organizations
 app.get('/organizations', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
 });
 
-// Ruta para Projects
 app.get('/projects', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/projects.html'));
 });
 
-// 4. Inicio del servidor
 app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
 });

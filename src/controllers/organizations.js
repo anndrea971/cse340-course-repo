715ca76954e5a749adbe343dee29fddc5f711864
.\.git <.\.gitignore>
2026-03-22 17:40:49 -0300

    please work

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,14 +1,23 @@
 import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
 import { getProjectsByOrganizationId } from '../models/projects.js';
 
+// 1. The main list page (This was the missing piece!)
+const showOrganizationsPage = async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
+};
+
+// 2. The details page (The one you already had)
 const showOrganizationDetailsPage = async (req, res) => {
     const organizationId = req.params.id;
     const organizationDetails = await getOrganizationDetails(organizationId);
     const projects = await getProjectsByOrganizationId(organizationId);
     const title = 'Organization Details';
 
-    res.render('organization', {title, organizationDetails, projects});
+    res.render('organization', { title, organizationDetails, projects });
 };
 
-// Export any controller functions
+// 3. Now both functions are defined, so this export will finally work!
 export { showOrganizationsPage, showOrganizationDetailsPage };
